a14cffec4fa75663ce483d619ab66f61a6894a1d
Uriel <masterllamas@hotmail.com>
2019-11-06 19:31:48 -0600

    Uriel: Se agrego la doble consulta

--- src/modelo/ConsultasProductos.java
+++ src/modelo/ConsultasProductos.java
@@ -22,16 +22,24 @@ public class ConsultasProductos extends Conexion{
         Connection Con = getConexion();
         try {
             PreparedStatement PS;
-            String Sql = "insert into producto, inventario (idproductos, nombre, marca, precio unitario, stock, categoria) values (?,?,?,?,?,?)";
+            String SQl = "insert into inventario(idproducto, stock, categoria) values (?,?,?)";
+            PS = Con.prepareCall(SQl);
+            PS.setInt(1,Modelo.getIdProductos());
+            PS.setInt(2,Modelo.getStock());
+            PS.setString(3, Modelo.getCategoria());
+
+            PS.execute();
+
+            String Sql = "insert into producto(nombre, marca, precio_unitario) values (?,?,?)";
             PS = Con.prepareCall(Sql);
-            PS.setInt(1, Modelo.getIdProductos());
-            PS.setString(2, Modelo.getNombre());
-            PS.setString(3, Modelo.getMarca());
-            PS.setFloat(4, Modelo.getPrecioUnitario());
-            PS.setInt(5,Modelo.getStock());
-            PS.setString(6, Modelo.getCategoria());
-            //Ejecutar la consulta
-            PS.executeUpdate();
+            PS.setString(1, Modelo.getNombre());
+            PS.setString(2, Modelo.getMarca());
+            PS.setFloat(3, Modelo.getPrecioUnitario());
+
+            PS.execute();
+
+
+
             return true;
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null,"ERROR: " +e);
