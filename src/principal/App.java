e95113c99336c2245bb24a14518143cff450afcb
daniela <daniela1pc@outlook.com>
2019-11-08 11:52:43 -0600

    Dany: Test de prueba daaa

--- src/principal/App.java
+++ src/principal/App.java
@@ -9,6 +9,8 @@ public class App {
 //        Splash TEST =new Splash();
         ControladorVistaMenu test = new ControladorVistaMenu();
         
+        System.out.println("hola prro!");
+        
         
     }
 }
