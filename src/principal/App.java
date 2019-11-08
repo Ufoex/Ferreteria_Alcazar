a874b06bd474541ec4020c67b953c3e78265eb06
unknown <210679605@VALLES-LAB06-31.academicos.valles.udg.mx>
2019-11-08 08:55:17 -0600

    Testeo para Dany

--- src/principal/App.java
+++ src/principal/App.java
@@ -8,5 +8,7 @@ public class App {
 
 //        Splash TEST =new Splash();
         ControladorVistaMenu test = new ControladorVistaMenu();
+        
+        System.out.println("Hola mundo");
     }
 }
